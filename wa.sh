2b1e41eadb259ecd8dfda8f734d28af03cac987d
Alex Szczuczko <alex@szc.ca>
2013-12-06 18:48:45 -0500

    Rewrite partially complete (up to result parsing)

--- wa.sh
+++ wa.sh
@@ -1,39 +1,119 @@
 #!/bin/bash
+
 #
-# by Sairon Istyar, 2012
-# distributed under the GPLv3 license
-# http://www.opensource.org/licenses/gpl-3.0.html
+# Copyright 2012 Sairon Istyar
+# Copyright 2013 Alex Szczuczko
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-[ -f ~/.wolfram_api_key ] && . ~/.wolfram_api_key
+#
+# Setup
+#
 
-# properly encode query
-q=$(echo ${*} | sed 's/+/%2B/g' | tr '\ ' '\+')
+# Stop if an error is encountered
+set -e
+# Stop if an undefined variable is referenced
+set -u
 
-# fetch and parse result
-result=$(curl -s "http://api.wolframalpha.com/v2/query?input=${q}&appid=${API_KEY}&format=plaintext")
+error () {
+    echo "$@" >&2
+}
 
-if [ -n "$(echo ${result} | grep 'Invalid appid')" ] ; then
-	echo "Invalid API key!"
-	echo "Get one at https://developer.wolframalpha.com/portal/apisignup.html"
-	echo -n 'Enter your WolframAlpha API key:'
-	read api_key
-	echo "API_KEY=${api_key}" >> ~/.wolfram_api_key
-	exit 1
+#
+# Load stored API key
+#
+
+key_path="$HOME/.wolfram_api_key"
+key_pattern='[A-Z0-9]{6}-[A-Z0-9]{10}'
+
+if [ -f "$key_path" ]
+then
+    # Load the contents of $key_path file as API_KEY
+    API_KEY="$(<"$key_path")"
+    if ! [[ "$API_KEY" =~ $key_pattern ]]
+    then
+        error "WolframAlpha API key read from file '$key_path' doesn't match the validation pattern"
+        exit 2
+    fi
+
+elif [ -e "$key_path" ]
+then
+    error "WolframAlpha API key path '$key_path' exists, but isn't a file."
+    exit 3
+
+else
+    # Only prompt if the shell is interactive
+    if [ -v PS1 ]
+    then
+        error "Cannot query without a WolframAlpha API key"
+        error "Get one at https://developer.wolframalpha.com/portal/apisignup.html"
+
+        API_KEY=""
+        while ! [[ "$API_KEY" =~ $key_pattern ]]
+        do
+            read -r -p "Enter an API key in the form '$key_pattern': " API_KEY
+        done
+
+        echo "$API_KEY" > "$key_path"
+        error "Key stored in '$key_path' for future use"
+
+    else
+        error "Cannot query without a WolframAlpha API key. Write the key to '$key_path'"
+        exit 4
+    fi
 fi
 
+#
+# Read the query
+#
+
+query="${@?You must specify a query to make}"
+
+#
+# Perform the query
+#
+
+result="$(curl -sS -G --data-urlencode "appid=$API_KEY" \
+                      --data-urlencode "format=plaintext" \
+                      --data-urlencode "input=$query" \
+                      "https://api.wolframalpha.com/v2/query")"
+
+#
+# Process the result of the query
+#
+
+if [[ "$result" =~ Invalid appid ]]
+then
+    error "WolframAlpha API has rejected the given API key"
+    error "Modify or remove the key stored in file '$key_path'"
+    exit 5
+fi
+
+
 result=`echo "${result}" \
-	| tr '\n' '\t' \
-	| sed -e 's/<plaintext>/\'$'\n<plaintext>/g' \
-	| grep -oE "<plaintext>.*</plaintext>|<pod title=.[^\']*" \
-	| sed 's!<plaintext>!!g; \
-		s!</plaintext>!!g; \
-		s!<pod title=.*!\\\x1b[1;36m&\\\x1b[0m!g; \
-		s!<pod title=.!!g; \
-		s!\&amp;!\&!' \
-	| tr '\t' '\n' \
-	| sed  '/^$/d; \
-		s/\ \ */\ /g'`
+    | tr '\n' '\t' \
+    | sed -e 's/<plaintext>/\'$'\n<plaintext>/g' \
+    | grep -oE "<plaintext>.*</plaintext>|<pod title=.[^\']*" \
+    | sed 's!<plaintext>!!g; \
+        s!</plaintext>!!g; \
+        s!<pod title=.*!\\\x1b[1;36m&\\\x1b[0m!g; \
+        s!<pod title=.!!g; \
+        s!\&amp;!\&!' \
+    | tr '\t' '\n' \
+    | sed  '/^$/d; \
+        s/\ \ */\ /g'`
 
 
 # print result
